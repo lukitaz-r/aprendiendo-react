4f98b0f69cf67e60de009319b94c42e3e9041e1f
Lukita <lukitaz.ramirezjajaja@gmail.com>
2024-05-20 23:05:15 -0300

    [ADD] Proyecto 03: Seguidor de cursor con React

---
+++ 03-cursorFollower/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [cords, setCords] = useState({x: 0, y: 0})
+
+  useEffect(() => {
+    const handleMove = (e) => {
+      const { clientX, clientY } = e
+      setCords({x: clientX, y: clientY})
+    }
+
+    if (enabled) window.addEventListener('pointermove', handleMove)
+
+    return () => window.removeEventListener('pointermove', handleMove)
+  }, [enabled])
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+    return () => document.body.classList.remove('no-cursor')
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        display: `${enabled ? 'block' : 'none'}`,
+        position: 'absolute',
+        background: '#00000080',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -15,
+        top: 0,
+        width: 40,
+        height: 40,
+        transform: `translate(${cords.x}px, ${cords.y}px)`
+      }}
+      
+      />
+      <h1>Cursor Follower w/ React.js</h1>
+      <button
+        onClick={() => setEnabled(!enabled)}
+      > 
+        {
+          enabled 
+            ? 'Disable cursor following' 
+            : 'Enable cursor following'
+        }
+      </button>
+    </>
+  )
+}
+
+export default App
